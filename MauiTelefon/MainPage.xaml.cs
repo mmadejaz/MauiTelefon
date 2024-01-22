cbe49426ae65f48b8822742ded63f0e485b36671
Mirosław Madej <miroslaw.madej@akademiazamojska.edu.pl>
2024-01-22 17:50:36 +0100

    Dodaj pliki projektów.

---
+++ MauiTelefon/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiTelefon
+{
+    public partial class MainPage : ContentPage
+    {
+        string numer = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClicked(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            numer = numer + btn.Text;
+            entNumer.Text = numer;
+            if(numer.Length > 10 )
+                entNumer.FontSize = 30;
+
+        }
+        private void btnCallClicked(object sender, EventArgs e)
+        {
+            lblTest.Text = "dzwonię pod numer: " + entNumer.Text;
+        }
+    }
+
+}
